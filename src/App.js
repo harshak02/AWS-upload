b4b73fea2e9f1fd4e166ffdefa07ef76b454f46a
harshak02 <shkancharla7777@gmail.com>
2023-05-14 16:39:53 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function UploadForm() {
+  const [file, setFile] = useState(null);
+  const [url, setUrl] = useState(null);
+  const [permUrl,setPermUrl] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Get a pre-signed URL for uploading the file
+    axios.get('/upload-url', { params: { filename: file.name } })
+      .then(response => {
+        const url = response.data.url;
+        setUrl(url);
+
+        // Use the pre-signed URL to upload the file
+        axios.put(url, file, {
+          headers: {
+            'Content-Type': file.type,
+          },
+        }).then(res => {
+
+          console.log('File uploaded successfully!');
+          axios.get('/perm-url',{ params: { filename: file.name } }).then(res =>{
+            setPermUrl(res.data);
+          }).catch(error => {
+            console.log(error);
+          });
+
+        }).catch(error => {
+          console.log(error);
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Upload File to Scan</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">Upload</button>
+        {url && (
+          <p>File uploaded to bucket : {url}</p>
+        )}
+        {permUrl && (
+          <p>File's permanent Url is : {permUrl}</p>
+        )}
+      </form>
     </div>
   );
 }
 
-export default App;
+export default UploadForm;
